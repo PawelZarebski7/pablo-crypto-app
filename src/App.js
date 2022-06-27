170f8c11474f0d37dd2b76c07abbdcaf0e36ffb3
Paweł Zarębski <pawelzarebski7@wp.pl>
2022-06-28 01:32:00 +0200

    my first crypto react app with api from coingecko

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Coins from './components/Coins';
+import Navbar from './components/Navbar';
+import Coin from './routes/Coin'
 
 function App() {
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoins(res.data)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+      <Routes>
+        <Route path='/' element={<Coins coins={coins}/>} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
